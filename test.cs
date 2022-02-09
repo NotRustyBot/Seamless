a2197c3aaa69db08f29b15698a98c34eb2916457
NotRustyBot <65828047+NotRustyBot@users.noreply.github.com>
2022-02-09 23:10:30 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,5 +1,30 @@
-static class Test{
-  public satic void Run(){
-    Game.ShowChatMessage("it works.");
-  }
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using SFDGameScriptInterface;
+
+
+namespace SFDScript
+{
+
+    public partial class GameScript : GameScriptInterface
+    {
+
+        /* CLASS STARTS HERE - COPY BELOW INTO THE SCRIPT WINDOW */
+
+        static class Test
+        {
+            public static void Run()
+            {
+                Game.ShowChatMessage("it works.");
+            }
+        }
+
+
+        /* CLASS ENDS HERE - COPY ABOVE INTO THE SCRIPT WINDOW */
+
+    }
 }
